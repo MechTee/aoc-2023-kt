45f4143b4cb5867942d87ca7d245fc8b6416b1a7
Mechti <mechti.oezdogan@exxeta.com>
2023-12-01 11:00:28 +0100

    do the second part :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,37 +2,38 @@ import java.io.File
 
 fun main() {
     val file = File("src/main/resources/input.txt")
-    val res = file.readLines().sumOf { combineFirstAndLastDigit(it) }
+    val numbers = arrayOf(Pair("one", 1), Pair("two", 2), Pair("three", 3), Pair("four", 4), Pair("five", 5), Pair
+        ("six", 6),Pair("seven", 7), Pair("eight", 8), Pair("nine", 9))
+    val res = file.readLines().sumOf { combineFirstAndLastDigit(it, numbers) }
     println("result is: $res")
 }
 
 
-fun combineFirstAndLastDigit(input: String): Int {
-    val firstDigit = getFirstDigit(input)
-    val lastDigit = getLastDigit(input)
+fun combineFirstAndLastDigit(input: String, numbers: Array<Pair<String, Int>>): Int {
+    val firstDigit = getFirstDigit(input, numbers)
+    val lastDigit = getLastDigit(input, numbers)
     val asString = (firstDigit.toString() + lastDigit.toString())
-    println("result of line $input is: $asString")
+    println("result of line $input with $firstDigit and $lastDigit is: $asString")
     return asString.toInt()
 }
 
-fun getFirstDigit(input: String): Int {
-    var index = 0
-    while (index < input.length) {
-        if (input[index].isDigit()) {
-            return input[index].toString().toInt()
-        }
-        index++
-    }
-    return -1
+fun getFirstDigit(input: String, numbers: Array<Pair<String, Int>>): Int {
+    val digit = input.first { it.isDigit() }
+    val indexOfDigit = input.indexOfFirst { it.isDigit() }
+    val indicesOfNumbers = numbers.map { input.indexOf(it.first) }
+    val minimumIndex = indicesOfNumbers.filter { it < indexOfDigit && it != -1 }.minOrNull() ?: return digit
+        .digitToInt()
+    return numbers[indicesOfNumbers.mapIndexed() { index, i -> Pair(index, i) }
+        .first() { it.second == minimumIndex }
+        .first].second
 }
 
-fun getLastDigit(input: String): Int {
-    var index = input.length - 1
-    while (index >= 0) {
-        if (input[index].isDigit()) {
-            return input[index].toString().toInt()
-        }
-        index--
-    }
-    return -1
+fun getLastDigit(input: String, numbers: Array<Pair<String, Int>>): Int {
+    val digit = input.last { it.isDigit() }
+    val indexOfDigit = input.indexOfLast { it.isDigit() }
+    val indicesOfNumbers = numbers.map { input.lastIndexOf (it.first) }
+    val minimumIndex = indicesOfNumbers.filter { it > indexOfDigit && it != -1 }.maxOrNull() ?: return digit.digitToInt()
+    return numbers[indicesOfNumbers.mapIndexed() { index, i -> Pair(index, i) }
+        .first() { it.second == minimumIndex }
+        .first].second
 }
