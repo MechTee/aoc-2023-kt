e257eafb634b85613aa334638bf20235fed1c25b
Mechti <mechti.oezdogan@exxeta.com>
2023-12-04 14:07:03 +0100

    do part 2 but this time it's not as ugly (but kinda magical)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,26 @@ import kotlin.math.pow
 
 fun main() {
     val file = File("src/main/resources/input.txt")
-    val cards = file.readLines()
-        .map {
-            it.split(":")[1].split("|")
-                .map { it2 ->
-                    it2.split(" ")
-                        .filter { it3 -> it3.isNotEmpty() }
-                        .map { it4 -> it4.toInt() }
-                        .toIntArray()
-                }
-        }.map { it2 ->
-            {
-                val intersection = it2[0].intersect(it2[1].toList())
-                (2.0).pow((intersection.size - 1).toDouble()).toInt()
-            }
-        }.sumOf { it() }
-    println("cards: $cards")
+    val input = file.readLines()
+    val solvePart1 = solvePart1(input)
+    println("Part 1: $solvePart1")
+    val solvePart2 = solvePart2(input)
+    println("Part 2: $solvePart2")
 }
+
+fun solvePart1(input: List<String>) = input
+    .map { it.substringAfter(": ").split(" | ")
+        .map { it.split(" ").filterNot { it.isBlank() }.map{it.toInt() } }
+    }.mapIndexed { i, l -> i to l.first().intersect(l.last().toSet()) }
+    .sumOf { (_, l) -> 2.0.pow(l.size - 1).toInt() }
+
+fun solvePart2(input: List<String>) = MutableList(input.size) { 1 }
+    .apply {
+        input
+            .map { it.substringAfter(": ").split(" | ")
+                .map { it.split(" ").filterNot { it.isBlank() } }
+            }.forEachIndexed { i, card -> (1..card.first().intersect(card.last().toSet()).size)
+                .filter { i + it < size }
+                .forEach { this[i + it] += this[i] }
+        }
+    }.sum()
